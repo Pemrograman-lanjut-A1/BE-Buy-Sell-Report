9cd2bf8608d10131d5d19b64b000b066d5ba1bc9
Fathan <fathan.naufal@ui.ac.id>
2024-04-06 20:48:20 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/besell/BeSellApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.besell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/besell/BeSellApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.besell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
