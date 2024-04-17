017e58c30261594570363702ff1048f1eaebc323
Fec16 <jasonkentwinata@gmail.com>
2024-04-17 20:36:44 +0700

    Add Cart skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/bebuy/BeBuyApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bebuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeBuyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/bebuy/controller/CartController.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.bebuy.controller;
+
+import id.ac.ui.cs.advprog.bebuy.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/listing", produces="application/json")
+@CrossOrigin(origins="*")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/add-item")
+    public void addItemToCart(@RequestBody CartItem cartItem) {
+    }
+
+    @GetMapping("/get-items")
+    public List<CartItem> getCartItems() {
+    }
+
+    @PostMapping("/update-item-quantity")
+    public void updateItemQuantity(@RequestParam long productId, @RequestParam int quantity) {
+    }
+
+    @DeleteMapping("/remove-item/{productId}")
+    public void removeItemFromCart(@PathVariable long productId) {
+    }
+}
