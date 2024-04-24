48ec7ae4cd992cdaa4408e0561d42c1b64350472
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-04-24 20:50:40 +0700

    [RED] ADD tests for UserReportStrategy

---
+++ src/test/java/id/ac/ui/cs/advprog/besell/strategy/UserReportStrategyTest.java
@@ -0,0 +1,42 @@
+package id.ac.ui.cs.advprog.besell.strategy;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import id.ac.ui.cs.advprog.besell.enums.ReportTargetType;
+import id.ac.ui.cs.advprog.besell.model.Report;
+import id.ac.ui.cs.advprog.besell.repository.ReportRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+public class UserReportStrategyTest {
+    @Mock
+    private ReportRepository reportRepository;
+
+    private UserReportStrategy userReportStrategy;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        userReportStrategy = new UserReportStrategy(reportRepository);
+    }
+
+    @Test
+    public void testExecute() {
+        String userId = "123";
+        List<Report> expectedReports = Collections.singletonList(new Report("222","345","desc", LocalDateTime.now(), userId, ReportTargetType.USER));
+        when(reportRepository.findByUserId(userId)).thenReturn(expectedReports);
+
+        List<Report> reports = userReportStrategy.execute(userId);
+
+        assertNotNull(reports);
+        assertEquals(1, reports.size());
+        verify(reportRepository).findByUserId(userId);
+    }
+}
