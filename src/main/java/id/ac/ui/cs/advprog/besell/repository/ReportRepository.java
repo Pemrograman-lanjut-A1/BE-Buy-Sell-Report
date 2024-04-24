afc8014baa272fa9883e1bedea809374bf9fa3c8
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-04-24 20:53:36 +0700

    [RED] Create Skeleton for ReportRepository

--- src/main/java/id/ac/ui/cs/advprog/besell/repository/ReportRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/repository/ReportRepository.java
@@ -1,4 +1,15 @@
 package id.ac.ui.cs.advprog.besell.repository;
 
+import id.ac.ui.cs.advprog.besell.model.Report;
+
+import java.util.List;
+
 public class ReportRepository {
+    public List<Report> findReportsByItemId(String itemId) {
+        return null;
+    }
+
+    public List<Report> findByUserId(String userId) {
+        return null;
+    }
 }
