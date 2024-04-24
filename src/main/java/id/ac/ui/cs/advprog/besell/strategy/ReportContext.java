e7955f9c9aefc3625c3f378de9c658b75405dd4c
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-04-24 20:53:17 +0700

    [RED] Create Skeleton for ReportContext

---
+++ src/main/java/id/ac/ui/cs/advprog/besell/strategy/ReportContext.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.besell.strategy;
+
+import id.ac.ui.cs.advprog.besell.model.Report;
+
+import java.util.List;
+
+public class ReportContext {
+    private ReportStrategy reportStrategy;
+
+    public void setReportStrategy(ReportStrategy reportStrategy) {
+
+    }
+
+    public List<Report> loadReports(String targetId) {
+        return null;
+    }
+}
