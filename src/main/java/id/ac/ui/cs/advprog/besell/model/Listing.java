a68a35724e4a28662f1da35732bd1697d0a5acd8
Fathan <fathan.naufal@ui.ac.id>
2024-04-24 10:40:02 +0700

    [FEAT] Add builder pattern skeleton to Listing model

--- src/main/java/id/ac/ui/cs/advprog/besell/model/Listing.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/model/Listing.java
@@ -1,17 +1,42 @@
 package id.ac.ui.cs.advprog.besell.model;
 
-import lombok.Getter;
-import lombok.Setter;
 import java.util.UUID;
 
-@Getter @Setter
 public class Listing {
-    private String listingId;
-    private String listingName;
-    private int listingPrice;
-    private int listingStock;
+    private String id;
+    private String name;
+    private int price;
+    private int stock;
+    private String description;
+    private String imageUrl;
 
-    public Listing(){
-        this.listingName = UUID.randomUUID().toString();
+    public Listing(ListingBuilder builder){
+
+        this.id = UUID.randomUUID().toString();
+        this.name = builder.name;
+        this.price = builder.price;
+        this.stock = builder.stock;
+        this.description = builder.description;
+        this.imageUrl = builder.imageUrl;
+    }
+
+    public static class ListingBuilder {
+        // Required parameters
+        private String name;
+        private int price;
+
+        // Optional parameters
+        private int stock;
+        private String description;
+        private String imageUrl;
+
+        public ListingBuilder(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public Listing build() {
+            return new Listing(this);
+        }
     }
 }
