9de89ca818f7fc7f0480bb41bd9e744d9915ded1
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-04-24 19:11:53 +0700

    [RED] Create Skeleton for Report Model

--- src/main/java/id/ac/ui/cs/advprog/besell/model/Report.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/model/Report.java
@@ -1,4 +1,22 @@
 package id.ac.ui.cs.advprog.besell.model;
 
+import id.ac.ui.cs.advprog.besell.enums.ReportTargetType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
 public class Report {
+    private String id;
+    private String authorId;
+    private String description;
+    private LocalDateTime reportDate;
+    private String targetId;
+    private ReportTargetType targetType;
+
+    public Report(String id, String authorId, String description,
+                  LocalDateTime reportDate, String targetId, ReportTargetType targetType) {
+
+    }
 }
