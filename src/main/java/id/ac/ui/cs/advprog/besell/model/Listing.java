b8562edcc2f6ce27b0c06aefc3b372d5a50030a9
Fathan <fathan.naufal@ui.ac.id>
2024-05-01 00:00:03 +0700

    [GREEN] Implement Listing model

--- src/main/java/id/ac/ui/cs/advprog/besell/model/Listing.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/model/Listing.java
@@ -1,27 +1,36 @@
 package id.ac.ui.cs.advprog.besell.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.UUID;
 
 @Getter @Setter
+@Entity
+@Table(name = "listing")
 public class Listing {
+    @Id
     private String id;
+
     private String name;
     private int price;
     private int stock;
     private String description;
     private String imageUrl;
+    private String sellerId;
 
+    public Listing(){
+
+    }
     public Listing(ListingBuilder builder){
-
         this.id = UUID.randomUUID().toString();
         this.name = builder.name;
         this.price = builder.price;
         this.stock = builder.stock;
         this.description = builder.description;
         this.imageUrl = builder.imageUrl;
+        this.sellerId = builder.sellerId;
     }
 
     public void setStock(int stock) {
@@ -41,6 +50,7 @@ public class Listing {
         private int stock;
         private String description;
         private String imageUrl;
+        private String sellerId;
 
         public ListingBuilder(String name, int price) {
             this.name = name;
@@ -59,6 +69,11 @@ public class Listing {
 
         public ListingBuilder setImageUrl(String imageUrl) {
             this.imageUrl = imageUrl;
+            return this;
+        }
+
+        public ListingBuilder setSellerId(String sellerId) {
+            this.sellerId = sellerId;
             return this;
         }
 
