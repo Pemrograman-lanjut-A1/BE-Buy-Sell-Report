452c4d1db6a91bbbf8396f3e10dd8a554c99b882
Fathan <fathan.naufal@ui.ac.id>
2024-05-01 00:00:44 +0700

    [GREEN] Implement Listing repository interface

--- src/main/java/id/ac/ui/cs/advprog/besell/repository/ListingRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/repository/ListingRepository.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.besell.repository;
 
 import id.ac.ui.cs.advprog.besell.model.Listing;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,46 +9,5 @@ import java.util.Iterator;
 import java.util.List;
 
 @Repository
-public class ListingRepository{
-    private List<Listing> listingData = new ArrayList<>();
-
-    public Listing create(Listing listing){
-        if(listing.getStock() < 0){
-            listing.setStock(0);
-        }
-        listingData.add(listing);
-        return listing;
-    }
-
-    public Listing delete(Listing listing){
-        listingData.remove(listing);
-        return listing;
-    }
-
-    public Listing update(Listing updatedListing){
-        if(updatedListing.getStock() < 0){
-            updatedListing.setStock(0);
-        }
-        for (int i = 0; i < listingData.size(); i++) {
-            Listing listing = listingData.get(i);
-            if(listing.getId().equals(updatedListing.getId())){
-                listingData.set(i, updatedListing);
-                return updatedListing;
-            }
-        }
-        return null;
-    }
-
-    public Listing findById(String id){
-        for(Listing listing : listingData){
-            if(listing.getId().equals(id)){
-                return listing;
-            }
-        }
-        return null;
-    }
-
-    public Iterator<Listing> findAll(){
-        return listingData.iterator();
-    }
+public interface ListingRepository extends JpaRepository<Listing, String> {
 }
