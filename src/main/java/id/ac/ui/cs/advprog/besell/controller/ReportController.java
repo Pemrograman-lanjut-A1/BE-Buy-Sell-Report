7d0db9b46b9e50bfb8212d99c94f2f7f46cfcf36
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-05-01 20:43:52 +0700

    [GREEN] Implement High-level networking with REST api

--- src/main/java/id/ac/ui/cs/advprog/besell/controller/ReportController.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/controller/ReportController.java
@@ -1,9 +1,14 @@
 package id.ac.ui.cs.advprog.besell.controller;
 
+import id.ac.ui.cs.advprog.besell.model.Report;
 import id.ac.ui.cs.advprog.besell.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/report", produces = "application/json")
@@ -12,24 +17,49 @@ public class ReportController {
     @Autowired
     ReportService reportService;
     @PostMapping("/create")
-    public String createReport() {
-        return "createReport";
+    public ResponseEntity<Report> createReport(@RequestBody Report report) {
+        return new ResponseEntity<>(reportService.createReport(report), HttpStatus.CREATED);
     }
-    @PostMapping("/update")
-    public String updateReport() {
-        return "updateReport";
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<Report> updateReport(@PathVariable String id, @RequestBody Report reportDetails) {
+        return ResponseEntity.ok(reportService.updateReport(id, reportDetails));
     }
-    @GetMapping("/get-all-report")
-    public String getAllReport() {
-        return "allReports";
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReport(@PathVariable String id) {
+        reportService.deleteReport(id);
+        return ResponseEntity.noContent().build();
     }
-    @GetMapping("/get-report-by-id")
-    public String getReportById() {
-        return "reportById";
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Report>> findReportById(@PathVariable String id) {
+        Optional<Report> report = reportService.findReportById(id);
+        if (report.isPresent()) {
+            return new ResponseEntity<>(report, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-    @DeleteMapping("/delete")
-    public String deleteReport() {
-        return "deleteReport";
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Report>> findAllReports() {
+        return ResponseEntity.ok(reportService.findAll());
+    }
+
+    @GetMapping("/items/{itemId}")
+    public ResponseEntity<List<Report>> findReportsByItemId(@PathVariable String itemId) {
+        return ResponseEntity.ok(reportService.findReportsByItemId(itemId));
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<Report>> findReportsByUserId(@PathVariable String userId) {
+        return ResponseEntity.ok(reportService.findReportsByUserId(userId));
+    }
+
+    @GetMapping("/authors/{authorId}")
+    public ResponseEntity<List<Report>> findReportsByAuthorId(@PathVariable String authorId) {
+        return ResponseEntity.ok(reportService.findReportsByAuthorId(authorId));
     }
 
 }
