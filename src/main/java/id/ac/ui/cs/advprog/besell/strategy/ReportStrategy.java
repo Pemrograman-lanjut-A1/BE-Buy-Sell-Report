18674edb3f8d637f8ea37974d735e28245d44968
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-05-01 20:42:51 +0700

    [GREEN] Implement ReportStrategy

--- src/main/java/id/ac/ui/cs/advprog/besell/strategy/ReportStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/strategy/ReportStrategy.java
@@ -1,9 +1,10 @@
 package id.ac.ui.cs.advprog.besell.strategy;
 
 import id.ac.ui.cs.advprog.besell.model.Report;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface ReportStrategy {
-    List<Report> execute(String targetId);
+    List<Report> fetchReports(String targetId);
 }
