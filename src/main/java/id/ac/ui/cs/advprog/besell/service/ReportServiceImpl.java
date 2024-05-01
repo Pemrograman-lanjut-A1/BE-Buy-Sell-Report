5bac4a8d7e6d8e377b17b58e3341ede0e76aeb55
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-05-01 20:40:57 +0700

    [GREEN] Implement ReportServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/besell/service/ReportServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/service/ReportServiceImpl.java
@@ -1,34 +1,66 @@
 package id.ac.ui.cs.advprog.besell.service;
 
 import id.ac.ui.cs.advprog.besell.model.Report;
+import id.ac.ui.cs.advprog.besell.repository.ReportRepository;
+import id.ac.ui.cs.advprog.besell.strategy.ReportContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReportServiceImpl implements ReportService{
-    @Override
-    public Report createReport() {
-        return null;
+    @Autowired
+    private ReportRepository reportRepository;
+    @Autowired
+    private ReportContext reportContext;
+
+    public Report createReport(Report report) {
+        reportRepository.save(report);
+        return report;
     }
 
-    @Override
-    public Report updateReport() {
-        return null;
+
+    public Report updateReport(String id, Report reportDetails) {
+        return reportRepository.findById(id)
+                .map(report -> {
+                    report.setDescription(reportDetails.getDescription());
+                    return reportRepository.save(report);
+                })
+                .orElseGet(() -> {
+                    reportDetails.setId(id);
+                    return reportRepository.save(reportDetails);
+                });
     }
 
-    @Override
-    public Report deleteReport() {
-        return null;
+
+
+    public void deleteReport(String id) {
+        reportRepository.deleteById(id);
     }
 
-    @Override
-    public Report getReportById() {
-        return null;
+    public Optional<Report> findReportById(String id) {
+        return reportRepository.findById(id);
     }
 
     @Override
     public List<Report> findAll() {
-        return null;
+        return reportRepository.findAll();
+    }
+
+    @Override
+    public List<Report> findReportsByItemId(String itemId) {
+        return reportContext.loadReports(itemId);
+    }
+
+    @Override
+    public List<Report> findReportsByUserId(String userId) {
+        return reportContext.loadReports(userId);
+    }
+
+    @Override
+    public List<Report> findReportsByAuthorId(String authorId) {
+        return reportContext.loadReports(authorId);
     }
 }
