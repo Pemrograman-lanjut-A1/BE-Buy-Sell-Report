1b664ddde33cf8514efa5628d80b0afc46279ac4
narendradzulqarnain <narendra.dz1107@gmail.com>
2024-05-01 20:42:37 +0700

    [GREEN] Implement UserReportStrategy

--- src/main/java/id/ac/ui/cs/advprog/besell/strategy/UserReportStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/strategy/UserReportStrategy.java
@@ -2,18 +2,22 @@ package id.ac.ui.cs.advprog.besell.strategy;
 
 import id.ac.ui.cs.advprog.besell.model.Report;
 import id.ac.ui.cs.advprog.besell.repository.ReportRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UserReportStrategy implements ReportStrategy {
+
     private ReportRepository reportRepository;
-
     public UserReportStrategy(ReportRepository reportRepository) {
-
+        this.reportRepository = reportRepository;
     }
 
+
     @Override
-    public List<Report> execute(String userId) {
-        return null;
+    public List<Report> fetchReports(String userId) {
+        return reportRepository.findByTargetId(userId);
     }
 }
