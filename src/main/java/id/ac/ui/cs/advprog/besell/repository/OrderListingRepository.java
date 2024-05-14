5b929c6e6e784e94d56a1d6db94bdd6c406fb910
Fathan <fathan.naufal@ui.ac.id>
2024-05-14 22:47:13 +0700

    [RED] Add OrderListing Service test

--- src/main/java/id/ac/ui/cs/advprog/besell/repository/OrderListingRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/besell/repository/OrderListingRepository.java
@@ -1,2 +1,7 @@
-package id.ac.ui.cs.advprog.besell.repository;public interface OrderListingRepository {
+package id.ac.ui.cs.advprog.besell.repository;
+
+import id.ac.ui.cs.advprog.besell.model.OrderListing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderListingRepository extends JpaRepository<OrderListing, String> {
 }
